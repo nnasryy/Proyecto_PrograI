5238cdeaf0d1d016b224e2f7ccb99e83b999a2c6
nnasryy <nasrywilliamsbali@gmail.com>
2025-05-06 19:02:08 -0600

    Commmit

--- src/williams_nasry_proyectotienda/Williams_Nasry_ProyectoTienda.java
+++ src/williams_nasry_proyectotienda/Williams_Nasry_ProyectoTienda.java
@@ -59,84 +59,88 @@ public class Williams_Nasry_ProyectoTienda {
         System.out.println("*** CAJA ***");
         System.out.print("Ingrese el monto en HNL que añadirá a Caja: ");
         double monto = lea.nextDouble();
-        lea.nextLine(); 
+        lea.nextLine();
         System.out.println("Su monto de caja actual es de: L." + monto);
         System.out.println("Presione Enter para volver al Menú...");
-        lea.nextLine(); 
+        lea.nextLine();
     }
 
     public static void ventas() {
         Scanner lea = new Scanner(System.in);
-       int tipocliente;
-    do{
-          System.out.println("                                                                                     ");
-        System.out.println("*** VENTAS ***");
-          System.out.println("                                                                                     ");
-        System.out.println(" *** Tipos de Cliente ***");
-        System.out.println("Cliente Tipo A - Puede comprar cualquier producto. ");
-        System.out.println("Cliente Tipo B - Solo puede comprar Azúcar, Avena o Trigo");
-        System.out.println("Cliente Tipo C- Solo puede comprar Maíz");
-        System.out.println("                                                                                     ");
-        System.out.println("Ingrese 1 si es un Cliente Tipo A");
-        System.out.println("Ingrese 2 si es un Cliente Tipo B");
-        System.out.println("Ingrese 3 si es un Cliente Tipo C");
-        System.out.println("Ingrese el Tipo de Cliente que es dependiendo del producto :");
-         tipocliente = lea.nextInt();
-         lea.nextLine(); 
-      
-       if (tipocliente<1|| tipocliente > 3 ){
-           System.out.println("Error. Opción no válida, vuelva a intentar.");
-       }
-    }while (tipocliente < 1 || tipocliente > 3 );
-        
-        
-  if (tipocliente == 1){
-      System.out.println("Usted es: Cliente Tipo A");
-  } else if (tipocliente == 2){
-      System.out.println("Usted es: Cliente Tipo B");
-  } else if (tipocliente == 3){
-      System.out.println("Usted es: Cliente Tipo C");
-  }else{
-      System.out.println("Error: debe ingresar un número del 1 al 3.");
-  }
-       
-int codigo;  
-do {
-        System.out.println("Productos: ");
-        System.out.println("Código 1. Azúcar - L.30");
-        System.out.println("Código 2. Avena - L.25");
-        System.out.println("Código 3. Trigo - L.32");
-        System.out.println("Código 4. Maíz - L.20");
-        System.out.println("Ingrese el codigo del producto: ");
-         codigo = lea.nextInt();
-         lea.nextLine(); 
-         
-         if (codigo < 1 || codigo > 4 ){
-             System.out.println("Error. Opción no válida, vuelva a intentar");
-         }
-         
-} while (codigo < 1 || codigo > 4 );
-        
- 
-        
-        if (codigo >= 1 || codigo <= 4 && tipocliente == 1){
-            System.out.println("El tipo de cliente con el producto es válido.");
-        }else if (codigo >=1 || codigo < 4 && tipocliente == 2) {
-            System.out.println("El tipo de cliente con el producto es válido.");
-        }else if (codigo == 4 && tipocliente == 3){
-            System.out.println("El tipo de cliente con el producto es válido");
-        }else{
-            System.out.println("Error. Opción no válida, vuelva a intentar");
+        int tipocliente;
+        do {
+            System.out.println("                                                                                     ");
+            System.out.println("*** VENTAS ***");
+            System.out.println("                                                                                     ");
+            System.out.println(" *** Tipos de Cliente ***");
+            System.out.println("Cliente Tipo A - Puede comprar cualquier producto. ");
+            System.out.println("Cliente Tipo B - Solo puede comprar Azúcar, Avena o Trigo");
+            System.out.println("Cliente Tipo C- Solo puede comprar Maíz");
+            System.out.println("                                                                                     ");
+            System.out.println("Ingrese 1 si es un Cliente Tipo A");
+            System.out.println("Ingrese 2 si es un Cliente Tipo B");
+            System.out.println("Ingrese 3 si es un Cliente Tipo C");
+            System.out.println("Ingrese el Tipo de Cliente que es dependiendo del producto :");
+            tipocliente = lea.nextInt();
+            lea.nextLine();
+
+            if (tipocliente < 1 || tipocliente > 3) {
+                System.out.println("Error. Opción no válida, vuelva a intentar.");
+            }
+        } while (tipocliente < 1 || tipocliente > 3);
+
+        if (tipocliente == 1) {
+            System.out.println("Usted es: Cliente Tipo A");
+        } else if (tipocliente == 2) {
+            System.out.println("Usted es: Cliente Tipo B");
+        } else if (tipocliente == 3) {
+            System.out.println("Usted es: Cliente Tipo C");
+        } else {
+            System.out.println("Error: debe ingresar un número del 1 al 3.");
         }
-        
-        System.out.println("Ingrese la cantidad en kilogramos que comprara: ");
-        double kilogramos = lea.nextDouble();
-        
-        
+
+        String respuesta;
+        do {
+            int codigo;
+
+            do {
+                System.out.println("Productos: ");
+                System.out.println("Código 1. Azúcar - L.30");
+                System.out.println("Código 2. Avena - L.25");
+                System.out.println("Código 3. Trigo - L.32");
+                System.out.println("Código 4. Maíz - L.20");
+                System.out.println("Ingrese el codigo del producto: ");
+                codigo = lea.nextInt();
+                lea.nextLine();
+
+                if (codigo < 1 || codigo > 4) {
+                    System.out.println("Error. Opción no válida, vuelva a intentar");
+                }
+
+            } while (codigo < 1 || codigo > 4);
+
+            if (codigo >= 1 && codigo <= 4 && tipocliente == 1) {
+                System.out.println("El tipo de cliente con el producto es válido.");
+            } else if (codigo >= 1 && codigo < 4 && tipocliente == 2) {
+                System.out.println("El tipo de cliente con el producto es válido.");
+            } else if (codigo == 4 && tipocliente == 3) {
+                System.out.println("El tipo de cliente con el producto es válido");
+            } else {
+                System.out.println("Error. Opción no válida, vuelva a intentar");
+            }
+
+            System.out.println("Ingrese la cantidad en kilogramos que comprara: ");
+            double kilogramos = lea.nextDouble();
+            lea.nextLine();
+
+            System.out.println("Desea Agregar otro producto?");
+            respuesta = lea.nextLine();
+
+        } while (respuesta.toLowerCase().equals("si"));
+
         System.out.println("Presione Enter para volver al Menú...");
-        lea.nextLine(); 
-        
-        
+        lea.nextLine();
+
     }
 
     public static void compras() {
